c6f5a15bbb87ab763cd33f6634fe394e933fb011
Szxrtskyy <szrtt123@gmail.com>
2025-09-29 12:33:12 +0200

    wylosowanieWartociDoTablicy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //losuj 10 liczb do tablicy
+
+        int[] tablicaWylosowanychLiczb = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < tablicaWylosowanychLiczb.length; i++) {
+            tablicaWylosowanychLiczb[i] = random.nextInt(100)+1;//od 1 do 101
+        }
+
+        //Wypisz elemnty tablicy
+
+        System.out.println("wylosowana tablica: ");
+        for (int i = 0; i < tablicaWylosowanychLiczb.length ; i++) {
+            System.out.println(tablicaWylosowanychLiczb[i]+ ", ");
+
+        }
+
+
+
     }
 }
